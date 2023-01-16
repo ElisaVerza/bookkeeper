1275c9c5efdff552e7f51bb24cf7a3537e38f063
ElisaVerza <velis.smile@gmail.com>
2023-01-16 11:42:18 +0100

    Test write + fixed test read

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoUtilityTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoUtilityTest.java
@@ -1,5 +1,9 @@
 package org.apache.bookkeeper.bookie;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,11 +17,13 @@ import org.apache.bookkeeper.common.util.Watcher;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -31,16 +37,27 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @RunWith(value=Parameterized.class)
 public class FileInfoUtilityTest{
     private FileInfo fi;
+    private FileInfo spyFi;
 
     private long firstLac;
     private long secondLac;
     private long singleLac;
     private int buffSize;
+    private int bbLen;
+    private long position;
     private long offset;
-    private String byteToWrite;
-    static String str = "Write something";
+    private String[] byteToWrite;
+    static String[] singleStr = {"Write something"};
+    static String[] multiStr = {"Write something", "and something else"};
     private boolean bestEffort;
-    private int size;
+    private Boolean isClosed;
+    private Boolean force;
+    private ByteBuffer[] bbArray;
+    private int writtenChar;
+    private ByteBuffer toWrite;
+
+
+
 
     private ByteBuffer bufferToRead;
 
@@ -49,24 +66,27 @@ public class FileInfoUtilityTest{
     
     @Parameters
     public static Collection<Object[]> getTestParameters(){
-        return Arrays.asList(new Object[][]{
-            {2l, 1l, 1l, 8, 0, true, str, str.length()}, //inserimento lac minore, besteffort true e size del buffer giusta
-            {1l, 2l, -1l, 16, 0, false, str, str.length()+100}, //inserimento lac maggiore, besteffort false e size del buffaer maggiore
-            {1l, 1l, 0l, 24, str.length()+100, true, str, str.length()}, //inserimento lac maggiore, indice maggiore lunghezza buffer
-            {1l, 1l, 0l, 24, -2000, true, str, str.length()}, //inserimento lac maggiore, indice negativo
-            {1l, 1l, 0l, 24, 0, true, "", 1}, //inserimento lac maggiore, file channel senza dati scritti*/
+        return Arrays.asList(new Object[][]{     
+            {2l, 1l, 1l, 8, 0, false, singleStr[0].length()+100, singleStr, true, false, 0}, //inserimento lac minore, besteffort false con buffer in cui copiare più grande della sringa, scrivo da inizio file
+            {1l, 1l, 0l, 24, singleStr[0].length()+100, true, singleStr[0].length(), singleStr, true, true, -2000}, //inserimento lac maggiore, indice maggiore lunghezza buffer, inizio a scrivere da indice negativo
+            {1l, 2l, -1l, 16, -2000, true, singleStr[0].length(), singleStr, false, false, 0}, //inserimento lac maggiore, indice negativo
+            {1l, 1l, 0l, 24, 0, false, singleStr[0].length(), multiStr, null, null, 1000}, //inserimento lac maggiore, besteffort false con buffer in cui copiare più piccolo della stringa da copiare, scrivo da un certo punto del file
+            //{1l, 1l, 0l, 24, 0, true, 0, null, null, null, 0}, //inserimento lac maggiore, file channel senza dati scritti
         });
     }
 
-    public FileInfoUtilityTest(long firstLac, long secondLac, long singleLac, int buffSize, long offset, boolean bestEffort, String byteToWrite, int size){
+    public FileInfoUtilityTest(long firstLac, long secondLac, long singleLac, int buffSize, long offset, boolean bestEffort, int bbLen, String byteToWrite[], Boolean force, Boolean isClosed, long position){
         this.firstLac = firstLac;
         this.secondLac = secondLac;
         this.singleLac = singleLac;
         this.buffSize = buffSize;
         this.offset = offset;
         this.bestEffort = bestEffort;
+        this.bbLen = bbLen;
         this.byteToWrite = byteToWrite;
-        this.size = size;
+        this.force = force;
+        this.isClosed = isClosed;
+        this.position = position;
     }
 /*Nel setup viene creato l'oggetto FileInfo e inizializzati gli oggetti mockati infine viene popolato l'header del file*/
     @Before
@@ -74,7 +94,6 @@ public class FileInfoUtilityTest{
         MockitoAnnotations.openMocks(this);
         int i = 0;
         byte[] headerMK = "SecondMK".getBytes();
-        bufferToRead = ByteBuffer.allocate(1024);
 
         File fl = new File(Variables.LEDGER_FILE_INDEX);
 
@@ -102,8 +121,28 @@ public class FileInfoUtilityTest{
         headerBB.rewind();
 
         myWriter.position(0);
-
+        spyFi = spy(fi);
     }
+
+    @Before
+    public void readWritebf(){
+        toWrite = ByteBuffer.allocate(0);
+        bbArray = new ByteBuffer[0];
+        writtenChar = 0;
+        int i = 0;
+        if(byteToWrite!=null){
+            bufferToRead = ByteBuffer.allocate(bbLen);
+            bbArray = new ByteBuffer[byteToWrite.length];
+            for(i=0; i<byteToWrite.length; i++){
+                writtenChar += byteToWrite[i].length();
+                toWrite = ByteBuffer.allocate(byteToWrite[i].length()); 
+                toWrite.put(ByteBuffer.wrap(byteToWrite[i].getBytes(UTF_8)));
+                toWrite.rewind();
+                bbArray[i] = toWrite;   
+            }
+        }
+    }
+
 /*Seguono due test per il metodo setLastAddConfirmed. Il metodo prende in input un long il cui valore rappresenta
  * l'indice last add confirmed (lac) ed in output il long con il lac aggiornato. Qualsiasi tipo di long è valido, 
  * ma il comportamento atteso è che venga rispettato un ordinamento, ovvero il lac viene aggiornato solo se è 
@@ -175,23 +214,52 @@ public class FileInfoUtilityTest{
     }
 
     @Test
+    public void writeTest() throws IOException{
+        if(position >= 0){
+            assertEquals(writtenChar, fi.write(bbArray, position));
+        }
+        else if(position < 0){
+            Assert.assertThrows(Exception.class, () -> fi.write(bbArray, position));   
+        }
+    }
+
+    @Test
     public void readTest() throws IOException{
-        ByteBuffer toWrite = ByteBuffer.allocate(size); 
-        toWrite.put(ByteBuffer.wrap(byteToWrite.getBytes(UTF_8)));
-        toWrite.rewind();
-        ByteBuffer[] bbArray = new ByteBuffer[1];
-        bbArray[0] = toWrite;
+
         fi.write(bbArray, 0);
 
-        if(offset>size || str.length()==0){
+        if(offset>writtenChar){
             assertEquals(0, fi.read(bufferToRead, offset, bestEffort));     
         }
-        else if(bestEffort & offset<size & offset>0){
+        if(bestEffort & offset<writtenChar & offset>=0){
             assertEquals(toWrite.capacity(), fi.read(bufferToRead, offset, bestEffort));
         }
-        else if((!bestEffort)|| offset<0){
+        else if(!bestEffort & bbLen <= toWrite.capacity()){
+            assertEquals(bbLen, fi.read(bufferToRead, offset, bestEffort));         
+        }
+        else if((!bestEffort & bbLen > toWrite.capacity())|| offset<0){
             Assert.assertThrows(Exception.class, () -> fi.read(bufferToRead, offset, bestEffort));
         }
+    }
+
+    @Test
+    public void closeTest() throws IOException{
+        if(isClosed!=null && force!=null){
+            spyFi.close(force);
+            if(isClosed){
+                spyFi.close(force);
+                verify(spyFi, times(1)).notifyWatchers(LastAddConfirmedUpdateNotification.FUNC, Long.MAX_VALUE);
+            }
+            else if(force){
+                verify(spyFi, times(1)).flushHeader();
+                verify(spyFi, times(1)).notifyWatchers(LastAddConfirmedUpdateNotification.FUNC, Long.MAX_VALUE);
+            }
+            else{
+                verify(spyFi, times(0)).flushHeader();
+                verify(spyFi, times(1)).notifyWatchers(LastAddConfirmedUpdateNotification.FUNC, Long.MAX_VALUE);
+            }
+        }
+
     }
 }
 
